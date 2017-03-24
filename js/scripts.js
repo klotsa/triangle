dfa599b73910d603dff6a99dc62f69c0e152b493
Anatoliy Klots and Marcel Nogueira <anatoliy@>
2017-03-23 18:05:08 -0700

    add styling and fix bugs

--- js/scripts.js
+++ js/scripts.js
@@ -4,21 +4,19 @@ $(document).ready(function() {
     var l1 = parseFloat($("input#firstside").val());
     var l2 = parseFloat($("input#secondside").val());
     var l3 = parseFloat($("input#thirdside").val());
-    debugger;
+    // debugger;
     if ( l1 + l2 <= l3 ||  l2 + l3 <=l1 || l3 + l1 <= l2 ) {
-      alert("if 1 work");
+      // alert("if 1 work");
         $("#notTriangle").show();
-    } else if ( l1 === l2 || l2 === l1 || l3 === l1 ){
-      alert("if 2 work");
+    } else if ( l1 === l2 && l2 === l1 && l3 === l1 ){
+      // alert("if 2 work");
         $("#equilateral").show();
-
     } else if (l1 != l2 && l2 != l3) {
-      alert("if 3 work");
+      // alert("if 3 work");
         $("#scalene").show();
     } else {
-      $("#isosceles").show(); 
+        $("#isosceles").show();
     }
     event.preventDefault();
-
   });
 });
